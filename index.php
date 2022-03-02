a802f9f575be27b75e08f170fd9ea1e5acc72e6d
Dugsteer <dugsteer@gmail.com>
2022-03-02 11:12:59 +0100

    Favicon update

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anglès Montalt</title>
+    <link rel="icon" type="image/png" href="img/favicon.png" ; />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
         integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
     <link rel="stylesheet" href="styles.min.css">

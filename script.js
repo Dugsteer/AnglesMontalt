e9ddff00fef9d48239c677aee9f630d8a3c1c250
Dugsteer <dugsteer@gmail.com>
2022-03-02 10:07:00 +0100

    PHP version

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const cover = document.getElementById("cover");
 const cover1 = document.getElementById("cover1");
 const loader = document.getElementById("expander");
 const navcontent = document.getElementById("navcontent");
+const booky2 = document.querySelector(".navcontent > .booky2text");
+
 const contact = false;
 
 function stopLoader() {
@@ -15,6 +17,13 @@ function stopLoader() {
 function startLoader() {
   loader.classList.remove("stop");
   setTimeout(stopLoader, 4000);
+}
+
+function displayTip() {
+  setTimeout(() => (booky2.style.display = "flex"), 100);
+}
+function displayCancel() {
+  booky2.style.display = "none";
 }
 
 setTimeout(stopLoader, 4000);
@@ -45,3 +54,12 @@ document.querySelectorAll(".fa-eye").forEach((eye) => {
     }
   });
 });
+
+navcontent.firstChild.nextElementSibling.firstChild.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.addEventListener(
+  "mouseover",
+  displayTip
+);
+navcontent.firstChild.nextElementSibling.firstChild.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.addEventListener(
+  "mouseout",
+  displayCancel
+);

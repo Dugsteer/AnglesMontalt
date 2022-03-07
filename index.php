d6f7d624e5287d3d57d6944c9cec6dee29899de8
Dugsteer <dugsteer@gmail.com>
2022-03-07 09:22:45 +0100

    Adding a count

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+
+include "./db_worksheets.php";
+
+global $connection;
+
+$query = " SELECT * FROM count ";
+$select_count = mysqli_query($connection, $query);
+
+while ($row = mysqli_fetch_assoc($select_count)) {
+    $views = $row['views'];
+}
+
+$newquery = "UPDATE count SET views = ${views}+1 ";
+$update_posts = mysqli_query($connection, $newquery);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 

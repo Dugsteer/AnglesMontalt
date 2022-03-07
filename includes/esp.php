a15e829b9cab108d891b352fcc4e66e54b8dbf4a
Dugsteer <dugsteer@gmail.com>
2022-03-07 11:47:58 +0100

    Multi lingual

---
+++ includes/esp.php
@@ -0,0 +1,272 @@
+        <div class="expander" id="expander">
+            <h4 id="pop">English en Sant Vicenç!</h4>
+            <ul class="popClass">
+                <li id="li1">Profesor Nativo</li>
+                <li id="li2">1-1 o Grupos Pequeños</li>
+                <li id="li3">Video o Presencial</li>
+                <li id="li4">La Primera Clase Gratis...</li><br>
+            </ul>
+            <!-- <div id="close" class="close">Tanca</div> -->
+        </div>
+
+        <!-- <div class="popup" id="popup"></div> -->
+
+
+        <div class="image-div">
+            <div class="image-inner"> <a href="?language=eng.php"><img src="img/UnionFlag.png"
+                        onclick="location.reload();" id="flag" alt=""></a>
+            </div>
+            <div class="image-inner"> <a href="?language=esp.php"><img src="img/spain-flag.png"
+                        onclick="location.reload();" id="flag" alt=""></a>
+            </div>
+            <div class="image-inner"> <a href="?language=cat.php"><img src="img/catalan-flag.png"
+                        onclick="location.reload();" id="flag" alt=""></a>
+            </div>
+        </div>
+        <div class="header">
+            <div class="header-left">
+                <p>AM</p>
+            </div>
+            <div class="header-right">
+                <p>Anglès Montalt</p>
+                <h5 id="number1">Whatsapp 666068734</h5>
+            </div>
+        </div>
+        <div class="navcontent booky2" id="navcontent">
+            <ul>
+                <li id="whose" onclick="whoIs();" class="underline">Quién Soy?</li>
+                <li id="contact" onclick="inTouch();" class="underline">Contacto</li>
+                <li class="underline" onclick="window.scrollTo(0, 900);">Qué Hago?</li>
+                <a href="https://esl-ology.com">
+                    <li class="underline">Esl-ology.com</li>
+                </a>
+
+            </ul>
+            <span class="booky2text" id="web">Un web gratis que he programado para profesores y
+                estudiantes</span>
+        </div>
+
+        <div class="topsdiv">
+            <div id="top2" class="top2">
+                <div class="piccy">
+                    <img src="img/DugWebpic.jpg" alt="">
+                    <p>
+                    <blockquote>"Hablar otra lengua es tener una segunda alma"—Carlomagno</blockquote>
+                    </p>
+                </div>
+                <p>Soy Dugald Steer y soy profesor licenciado en inglés.Tengo 15 años de experiencia con todos los
+                    niveles hasta IELTS y Cambridge
+                    Advanced.Tanto si eres principiante, utiliza el inglés en el trabajo, necesitas refuerzo escolar o
+                    tienes un examen,te ofrezco un programa a medida que garantiza los mejores resultados y una
+                    experiencia encantadora.
+                </p>
+                <div class="booky" data-tooltip="También escribo libros. Éstos son algunos ejemplos">
+
+                    <img src="img/OlogySeries.jpg" alt="">
+
+
+                </div>
+
+            </div>
+
+            <div id="top1" class="top1" name="top1">
+                <div class="left1">
+                    <p>Formulario de Contacto</p>
+
+
+                    <form id="form-id" class="form-class" method="post" action="contact-form-process.php">
+
+                        <div class="form-group">
+                            <label for="Name" class="label">Nombre</label>
+                            <div class="input-group">
+                                <input type="text" id="Name" name="Name" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="Email" class="label">Email</label>
+                            <div class="input-group">
+                                <input type="email" id="Email" name="Email" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="Message" class="label">Mensage</label>
+                            <div class="input-group">
+                                <textarea id="Message" name="Message" class="form-control" rows="6" maxlength="3000"
+                                    required></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" id="button" class="btn btn-primary btn-lg btn-block">Enviar
+                                mensage</button>
+                        </div>
+                    </form>
+                    <p>
+                    <blockquote>Hago clases en mi casa, en tu casa or por internet. Dondé quieras!</blockquote>
+                    </p>
+
+                </div>
+                <div class="contact">
+                    <p class="details">Pónte en contacto conmigo mediante este formulario o enviando un whatsapp a
+                        666068734 y le contestaré lo antes posible. La primera clase de prueba es gratis. Mi dirección
+                        es:<br><br>Dugald Steer <br> Carrer Costa Daurada 5, <br> Sant Vicenç de Montalt, <br>
+                        08394 Barcelona
+                    </p>
+                </div>
+
+                <div class="right1">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1492.5603196920222!2d2.515591200798547!3d41.566633567011884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x525adaf767e02e5d!2sAngl%C3%A8s%20Montalt!5e0!3m2!1sen!2ses!4v1602058352261!5m2!1sen!2ses"
+                        width="400" height="300" frameborder="0" style="border:1x solid black;" allowfullscreen=""
+                        aria-hidden="false" tabindex="0"></iframe>
+                </div>
+            </div>
+        </div>
+
+        <div class="content">
+
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-front">
+                        <div class="img-container">
+                            <img src="img/girl.jpg" alt="" class="nens">
+                        </div>
+                        <h2>Niños</h2>
+                        <i class="fas fa-child"></i>
+                        <ul class="list">
+                            <li class="list-item">Grupos Pequeños</li>
+                            <li class="list-item">Refuerzo del Libro Escolar</li>
+                            <li class="list-item">Juegos y Actividades</li>
+                            <li class="list-item">Videos y Canciones</li>
+                            <li class="list-item">Juegos En linea </li>
+                            <li class="list-item">y Hablar un Monton!</li>
+                        </ul>
+                    </div>
+                    <div class="card-back">
+                        <ul class="list">
+                            <li class="list-item">Grupos Pequeños</li>
+                            <li class="list-item">Refuerzo del Libro Escolar</li>
+                            <li class="list-item">Juegos y Actividades</li>
+                            <li class="list-item">Videos y Canciones</li>
+                            <li class="list-item">Juegos En linea </li>
+                            <li class="list-item">y Hablar un Monton!</li>
+                        </ul>
+                        <div class="img-holder">
+                            <div class="img-container-back">
+                                <img src="img/girl-min.jpg" alt="" class="nens2">
+                            </div>
+                            <span data-tooltip="Más Info" class="span1"><i class="fas fa-eye"></i></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-front">
+                        <div class="img-container">
+                            <img src="img/online.jpg" alt="" class="nens">
+                        </div>
+                        <h2>Zoom</h2>
+                        <i class="fas fa-laptop"></i>
+                        <ul class="list">
+                            <li class="list-item">Clases Quando Quieras</li>
+                            <li class="list-item">1-1 o Grupos</li>
+                            <li class="list-item">Juegos y Actividades</li>
+                            <li class="list-item">A Tu Ritmo</li>
+                            <li class="list-item">Packs Economicos</li>
+                            <li class="list-item">y Hablar un Monton!</li>
+                        </ul>
+                    </div>
+                    <div class="card-back">
+                        <ul class="list">
+                            <li class="list-item">Clases Quando Quieras</li>
+                            <li class="list-item">1-1 o Grupos</li>
+                            <li class="list-item">Juegos y Actividades</li>
+                            <li class="list-item">A Tu Ritmo</li>
+                            <li class="list-item">Packs Economicos</li>
+                            <li class="list-item">y Hablar un Monton!</li>
+                        </ul>
+                        <div class="img-holder">
+                            <div class="img-container-back">
+                                <img src="img/online-min.jpg" alt="" class="nens2">
+                            </div>
+                            <span data-tooltip="Más Info" class="span1"><i class="fas fa-eye"></i></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-front">
+                        <div class="img-container">
+                            <img src="img/business.jpg" alt="" class="nens">
+                        </div>
+                        <h2>Empresas</h2>
+                        <i class="fas fa-chart-bar"></i>
+                        <ul class="list">
+                            <li class="list-item">1-1 o Grupos</li>
+                            <li class="list-item">Experiencia en 'Business'</li>
+                            <li class="list-item">Para Reuniones</li>
+                            <li class="list-item">Para Llamadas</li>
+                            <li class="list-item">Para Email, Cartas etc</li>
+                            <li class="list-item">y Hablar un Monton!</li>
+                        </ul>
+                    </div>
+                    <div class="card-back">
+                        <ul class="list">
+                            <li class="list-item">1-1 o Grupos</li>
+                            <li class="list-item">Experiencia en 'Business'</li>
+                            <li class="list-item">Para Reuniones</li>
+                            <li class="list-item">Para Llamadas</li>
+                            <li class="list-item">Para Email, Cartas etc</li>
+                            <li class="list-item">y Hablar un Monton!</li>
+                        </ul>
+                        <div class="img-holder">
+                            <div class="img-container-back">
+                                <img src="img/business-min.jpg" alt="" class="nens2">
+                            </div>
+                            <span data-tooltip="Más Info" class="span1"><i class="fas fa-eye"></i></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-front">
+                        <div class="img-container">
+                            <img src="img/adults.jpg" alt="" class="nens">
+                        </div>
+                        <h2>Adultos</h2>
+                        <i class="fas fa-user"></i>
+                        <ul class="list">
+                            <li class="list-item">Grupos Pequeños</li>
+                            <li class="list-item">Clases Particulares</li>
+                            <li class="list-item">Examens de Cambridge</li>
+                            <li class="list-item">Preparación para Entrevistas</li>
+                            <li class="list-item">Progreso Rápido</li>
+                            <li class="list-item">y Hablar un Monton!</li>
+                        </ul>
+                    </div>
+                    <div class="card-back">
+                        <ul class="list">
+                            <li class="list-item">Grupos Pequeños</li>
+                            <li class="list-item">Clases Particulares</li>
+                            <li class="list-item">Examens de Cambridge</li>
+                            <li class="list-item">Preparación para Entrevistas</li>
+                            <li class="list-item">Progreso Rápido</li>
+                            <li class="list-item">y Hablar un Monton!</li>
+                        </ul>
+                        <div class="img-holder">
+                            <div class="img-container-back">
+                                <img src="img/adults-min.jpg" alt="" class="nens2">
+                            </div>
+                            <span data-tooltip="Más Info" class="span1"><i class="fas fa-eye"></i></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
